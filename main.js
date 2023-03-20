9d7683a57f1103b335293106eb51031f922321fa
CannelleJP <isaibmezghiche@edenschool.fr>
2023-03-20 14:42:15 +0100

    finish exo 6

---
+++ main.js
@@ -0,0 +1,133 @@
+
+
+
+// exo 1
+
+// let i = 0
+// let result = ""
+// while (i != 11) {
+
+// result += i
+
+//     i++
+
+   
+    
+
+
+// }  
+
+// console.log(result);
+   
+// let result = ""
+// for (let i = 0; i <= 10; i++) {
+  
+//   result += i
+  
+// }
+
+// console.log(result);
+
+
+
+
+
+
+/*
+
+// exo 2
+
+let i = 0
+let result = ""
+while (i != 11) {
+
+result += `${i} `
+
+    i++
+
+   
+    
+
+
+}  
+
+console.log(result);
+
+*/
+
+
+/*
+
+// exo 3
+
+
+let result = ""
+
+for (let i = 0; i <= 100 ; i++) {
+
+
+
+
+if (i % 2 != 0) {
+  result += `${i} `
+}
+
+  
+}
+console.log(result);*/
+
+
+
+
+/*
+
+// exp 4
+let result = ""
+for (let i = 0; i <= 10; i++) {
+
+  result +=  `${i*7} `
+  
+}
+
+console.log(result);*/
+
+
+
+/*
+
+//exo 5
+
+let result = ""
+
+
+
+
+for (let i = 1; i <= 10; i++) {
+
+  
+
+for (let j = 1; j <= 10; j++) {
+  result +=  `${i*j} `
+  
+}
+
+
+}
+
+console.log(result);*/
+
+
+function somme() {
+  let result = 0
+
+  for (let i = 0; i <= 10; i++) {
+ 
+    result += i
+    
+  }
+
+  return result
+}
+
+
+console.log(somme());
